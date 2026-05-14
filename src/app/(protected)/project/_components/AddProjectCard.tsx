27a3dd3a06f809bf6272ee5117bfa9e6c2cbb1da
Yasmin Hillis <.com>
2026-05-14 03:27:19 +0300

    fix add project card click

--- src/app/(protected)/project/_components/AddProjectCard.tsx
+++ src/app/(protected)/project/_components/AddProjectCard.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 export default function AddProjectCard(){
     const router = useRouter(); 
     
-    return <div className="hidden md:flex flex-col items-center justify-center h-[220px] bg-white w-full max-w-[304px] rounded-md p-6 border-2 border-dotted border-[#C3C6D633]">
+    return <div onClick={() => router.push('/project/add')} className="hidden md:flex flex-col items-center justify-center h-[220px] bg-white w-full max-w-[304px] rounded-md p-6 border-2 border-dotted border-[#C3C6D633] cursor-pointer">
 
         <div onClick={() => router.push('/project/add')} className="bg-[#F1F3FF] cursor-pointer w-[48px] h-[48px] rounded-lg flex items-center justify-center mb-4">
             <span onClick={() => router.push('/project/add')} className="material-symbols-outlined cursor-pointer">
