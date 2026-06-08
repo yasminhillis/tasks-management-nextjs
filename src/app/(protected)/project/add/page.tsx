b6d53c004b601fa68fc021bacdb463b392793f8f
Yasmin Hillis <.com>
2026-06-08 03:50:22 +0300

    remove commented file and use Header and PageWrapper reusable components

--- src/app/(protected)/project/add/page.tsx
+++ src/app/(protected)/project/add/page.tsx
@@ -1,26 +1,18 @@
+import Header from '../_components/Header';
+import PageWrapper from '../_components/PageWrapper';
 import ProjectForm from '../_components/ProjectForm';
 
 export default function AddProject() {
   return (
-    <div className="font-sans px-6 pt-8 md:px-8 md:pt-6 mb-30 min-h-screen">
-      <div className="md:hidden mb-8">
-        <h2 className="md:hidden text-[#041B3C] font-semibold text-2xl mb-1">
-          Initialize New Project
-        </h2>
-        <p className="text-[#4F5F7B] text-sm">
-          Define the scope and foundational details of your project.
-        </p>
-      </div>
-      <div className="hidden md:flex items-center justify-between mb-10">
-        <h2 className="hidden md:block font-semibold md:text-4xl text-[#041B3C]">
-          Add New Project
-        </h2>
-        <button className="hidden md:flex items-center gap-2 px-6 py-3 shadow-blue-md bg-radial from-[#003D9B] to-[#0052CC] text-white text-sm cursor-pointer hover:from-[#1259cb] hover:to-[#0657d1] transition-colors font-bold">
-          <span className="material-symbols-outlined">person_add</span>
-          Invite Member
-        </button>
-      </div>
+    <PageWrapper>
+      <Header
+        mobileTitle="Initialize New Project"
+        mobileDescription="Define the scope and foundational details of your project."
+        desktopTitle="Add New Project"
+        buttonLabel="Invite Member"
+        materialIcon="person_add"
+      />
       <ProjectForm mode="add" />
-    </div>
+    </PageWrapper>
   );
 }
--- src/app/(protected)/project/page.tsx
+++ src/app/(protected)/project/page.tsx
@@ -1,15 +1,12 @@
 import InfiniteScroll from '../_components/InfiniteScroll';
 import PageWrapper from './_components/PageWrapper';
-// import Pagination from './_components/Pagination';
 import ProjectsList from './ProjectsList';
 
 export default function Project() {
   return (
-    // <div className="mb-[88px] md:mb-[162px]">
     <PageWrapper>
       <ProjectsList />
       <InfiniteScroll slice="projects" />
     </PageWrapper>
-    // </div>
   );
 }
