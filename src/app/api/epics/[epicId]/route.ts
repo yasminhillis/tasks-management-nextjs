bea5a540f142e44902d19806ec35427491cab977
Yasmin Hillis <.com>
2026-06-09 22:39:15 +0300

    feat: add GET /api/epics/[epicId] route

---
+++ src/app/api/epics/[epicId]/route.ts
@@ -0,0 +1,43 @@
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+function getHeaders(token: string | undefined){
+    return {
+        'Authorization': `Bearer ${token}`, 
+        apikey: process.env.SUPABASE_ANON_KEY!,
+        'Content-Type': 'application/json'
+    }
+}
+
+export async function GET(req: NextRequest,{params}: {params: { epicId: string }}){
+    const projectId = req.nextUrl.searchParams.get('projectId');
+    // const projectId ='ca4443f9-f0f9-416c-8252-34833c48a01'
+    const {epicId} = await params;
+    console.log(epicId, 'epicId');
+
+    const cookieStore = await cookies();
+    const token = await cookieStore.get('access_token')?.value;
+    try {
+        const res = await fetch(`${process.env.SUPABASE_URL}/rest/v1/project_epics?project_id=eq.${projectId}&id=eq.${epicId}`, {
+            headers: getHeaders(token)
+        })
+
+        if (!res.ok) {
+            const error = await res.json();
+            console.log(error, 'error'); 
+            return Response.json({ success: false, message: 'Something went wrong' }, { status: 404 }) 
+        }
+        const result = await res.json();
+
+        if (result.length === 0) {
+            return Response.json({ success: false, message: 'Epic not found' }, { status: 404 }) 
+        }
+
+        console.log(result, 'result');
+        
+        return Response.json({ success: true, message: 'Epic details fetched successfully', epic: result[0] }, { status: 200 })
+
+    } catch(error){
+        return Response.json({success: false, error: error}, { status: 404 })
+    }
+}
