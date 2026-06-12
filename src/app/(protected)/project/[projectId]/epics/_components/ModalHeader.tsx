e15db05f58218b5e2253ea78fb7efec8cc5f9234
Yasmin Hillis <.com>
2026-06-12 19:13:36 +0300

    TM-18 style: make epic detail popup responsive

--- src/app/(protected)/project/[projectId]/epics/_components/ModalHeader.tsx
+++ src/app/(protected)/project/[projectId]/epics/_components/ModalHeader.tsx
@@ -7,11 +7,12 @@ type ModalHeaderProps = {
 }
 
 export default function ModalHeader({epicId, title, onClose}: ModalHeaderProps){
-    return <div className="p-[32px] flex justify-between border-b border-b-[#C3C6D626]">
+    return <div className="bg-linear-to-b from-white to-[#F1F3FF] md:bg-none bg-white pt-[24px] pr-[24px] pb-[8px] pl-[24px] md:p-[32px] flex justify-between border-b border-b-[#C3C6D626]">
         <div>
             <div className="flex items-center gap-[8px] mb-[8px]">
                   <EpicIcon />
-                <p className="text-[12px] font-bold tracking-[0.6px] uppercase opacity-60">{epicId}</p>
+                <p className="text-[10px] font-bold uppercase tracking-[0.06em] text-[#003D9B] 
+              md:text-[12px] md:tracking-[0.6px] md:text-[#041B3C] md:opacity-60">{epicId}</p>
             </div>
             <h1 className="title-xl">{title}</h1>
         </div>
