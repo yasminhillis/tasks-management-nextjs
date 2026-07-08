c6bbffc9184e4372431ee0a0d399bcc938bb9798
Yasmin Hillis <.com>
2026-07-08 20:04:50 +0300

    TM-20 feat: add addNewTask server action with Supabase POST

---
+++ src/lib/actions/taskActions.ts
@@ -0,0 +1,40 @@
+'use server'
+
+import { getApiHeaders } from "../utils/getApiHeaders";
+
+type Task = {
+    project_id: string,
+    assignee_id: string
+    description: string
+    due_date: string
+    epic_id: string
+    status: string
+    title: string
+}
+
+export async function addTask(data: Partial<Task>) {
+    try {
+        console.log(process.env.SUPABASE_URL, 'url');
+
+        const res = await fetch(`${process.env.SUPABASE_URL}/rest/v1/tasks`, {
+            method: 'POST',
+            headers: await getApiHeaders(),
+            body: JSON.stringify({
+                project_id: data.project_id,
+                assignee_id: data.assignee_id,
+                description: data.description,
+                due_date: data.due_date,
+                epic_id: data.epic_id,
+                status: data.status,
+                title: data.title
+            })
+        })
+
+        if (!res.ok) {
+            return { success: false, message: 'Something went wrong. Please try again' }
+        }
+        return { success: true, message: 'Task added successfully' }
+    } catch (error) {
+        return { success: false, message: 'Network error. Please check you connection' }
+    }
+}
