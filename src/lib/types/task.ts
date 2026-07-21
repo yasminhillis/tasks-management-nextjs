e6a35175b9c11e4f6f262a1b5ba265cbb099298c
Yasmin Hillis <.com>
2026-07-21 04:33:58 +0300

    tm-21 fix(display-tasks-in-modal): implement the ui for task in modal

--- src/lib/types/task.ts
+++ src/lib/types/task.ts
@@ -1,4 +1,6 @@
 export enum Status { TO_DO = 'TO_DO', IN_PROGRESS = 'IN_PROGRESS', BLOCKED = 'BLOCKED', IN_REVIEW = 'IN_REVIEW', READY_FOR_QA = 'READY_FOR_QA', REOPENED = 'REOPENED', READY_FOR_PRODUCTION = 'READY_FOR_PRODUCTION', DONE = 'DONE' }
+import type { Member } from "./members"
+import type { Epic } from "./epic";
 
 export type Task = {
     project_id: string
@@ -7,5 +9,20 @@ export type Task = {
     description?: string
     due_date?: Date | null
     epic_id?: string
-    status?:  Status
+    status?: Status
+}
+
+export type EpicTask = {
+    assignee: Pick<Member, 'email' | 'name' | 'role' & {id : string}>,
+    created_at: string
+    created_by: Member,
+    description: string,
+    due_date: string,
+    epic: Pick<Epic, 'epic_id' | 'title' | 'id'>,
+    epic_id: string,
+    id: string,
+    project_id: string,
+    status: string,
+    task_id: string,
+    title: string
 }
